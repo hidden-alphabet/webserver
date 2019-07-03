8535cde117e6756c0347f913d5414ae942b38dd7
Cole Hudson <cole@colejhudson.com>
2019-07-03 14:17:29 -0700

    add server skeleton

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+  "os"
+  "log"
+  "fmt"
+  "net/http"
+  "io/ioutil"
+  "database/sql"
+  _ "github.com/lib/pq"
+  "github.com/gorilla/mux"
+  "github.com/gorilla/handlers"
+)
+
+
+
+func resolveDatabaseURL(env string) string {
+  template := "postgres://%s:%s@%s:%s/%s?sslmode=disable"
+
+  switch env {
+  case "production":
+    host := os.Getenv("PG_HOST")
+    port := os.Getenv("PG_PORT")
+    username := os.Getenv("PG_USERNAME")
+    password := os.Getenv("PG_PASSWORD")
+    database := os.Getenv("PG_DATABASE")
+
+    return fmt.Sprintf(template, username, password, host, port, database)
+  case "development":
+    fallthrough
+  default:
+    username, exists := os.LookupEnv("PG_USERNAME")
+    if !exists {
+      username = "postgres"
+    }
+    password, exists := os.LookupEnv("PG_PASSWORD")
+    if !exists {
+      password = ""
+    }
+    database, exists := os.LookupEnv("PG_DATABASE")
+    if !exists {
+      database = "postgres"
+    }
+
+    return fmt.Sprintf(template, username, password, "localhost", "5432", database)
+  }
+}
+
+func main() {
+  env, exists := os.LookupEnv("ENV")
+  if !exists {
+    log.Println("Could not determine environment. Defaulting to development.")
+    env = "development"
+  }
+
+  url := resolveDatabaseURL(env)
+  db, err := sql.Open("postgres", url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  log.Println("Connected to database.")
+
+  router := mux.NewRouter()
+  http.Handle("/", handlers.LoggingHandler(os.Stdout, router))
+  log.Println("Server started at 0.0.0.0:8081.")
+
+  http.ListenAndServe(":8081", nil)
+}
