507dacc6d5971a389ead0bc5e91c09be682511a4
Cole Hudson <cole@colejhudson.com>
2019-07-02 22:43:29 -0700

    made host argument variable

--- db/init.sh
+++ db/init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -z $PG_HOST ]]; then
+  echo "[!] PG_HOST must be set."
+  exit -1
+fi
+
 if [[ -z $PG_DATABASE ]]; then
   echo "[!] PG_DATABASE must be set."
   exit -1
@@ -15,6 +20,11 @@ if [[ -z $PG_PASSWORD ]]; then
   exit -1
 fi
 
+if [[ -f /.dockerenv ]]; then
+  echo "[!] Waiting for database to start up"
+  sleep 2
+fi
+
 CREATE_USER="CREATE USER $PG_USER WITH PASSWORD '$PG_PASSWORD' CREATEDB"
 
 if [[ $PG_PASSWORD -eq default ]]; then
@@ -23,16 +33,16 @@ if [[ $PG_PASSWORD -eq default ]]; then
 fi
 
 echo "[!] creating user '$PG_USER'"
-psql -h 0.0.0.0 -p 5432 -U postgres -d postgres -c "$CREATE_USER;" 
+psql -h $PG_HOST -p 5432 -U postgres -d postgres -c "$CREATE_USER;"
 
 echo "[!] creating database '$PG_DATABASE'"
-psql -h 0.0.0.0 -p 5432 -U postgres -d postgres -c "CREATE DATABASE $PG_DATABASE WITH OWNER $PG_USER;"
+psql -h $PG_HOST -p 5432 -U postgres -d postgres -c "CREATE DATABASE $PG_DATABASE WITH OWNER $PG_USER;"
 
 echo "[!] creating schema"
-psql -h 0.0.0.0 -p 5432 -U $PG_USER -d $PG_DATABASE < schema.sql
+psql -h $PG_HOST -p 5432 -U $PG_USER -d $PG_DATABASE < schema.sql
 
 echo "[!] creating tables"
-psql -h 0.0.0.0 -p 5432 -U $PG_USER -d $PG_DATABASE < tables.sql
+psql -h $PG_HOST -p 5432 -U $PG_USER -d $PG_DATABASE < tables.sql
 
 echo "[!] removing public access to database"
-psql -h 0.0.0.0 -p 5432 -U $PG_USER -d $PG_DATABASE < security.sql
+psql -h $PG_HOST -p 5432 -U $PG_USER -d $PG_DATABASE < security.sql
